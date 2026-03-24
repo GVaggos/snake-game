01871d58b2d1991a57df842148903b60447d3e2a
Vaggelis <tzanogvagelakis@gmail.com>
2026-03-24 18:56:09 +0200

    added basic movement with arrow keys

--- main.py
+++ main.py
@@ -2,8 +2,22 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode((600, 400))
+width = 600
+height = 400
+
+screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Snake Game")
+
+# θέση
+x = 300
+y = 200
+
+# ταχύτητα
+speed = 5
+
+# κατεύθυνση
+dx = 0
+dy = 0
 
 running = True
 
@@ -12,7 +26,27 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                dx = -speed
+                dy = 0
+            if event.key == pygame.K_RIGHT:
+                dx = speed
+                dy = 0
+            if event.key == pygame.K_UP:
+                dy = -speed
+                dx = 0
+            if event.key == pygame.K_DOWN:
+                dy = speed
+                dx = 0
+
+    x += dx
+    y += dy
+
     screen.fill((0, 0, 0))
+
+    pygame.draw.rect(screen, (0, 255, 0), (x, y, 20, 20))
+
     pygame.display.update()
 
 pygame.quit()
